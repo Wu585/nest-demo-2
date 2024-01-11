9e820bd0934cfafd273e54ed3ffae511c0fb309b
Wu <15151851516@163.com>
2024-01-11 22:31:28 +0800

    tmp

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, Inject} from '@nestjs/common';
+import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('Test') private readonly testValue: string[],
+    @Inject("Factory") private readonly factory: number
+  ) {
+  }
 
   @Get()
   getHello(): string {
+    console.log(this.testValue)  // a b c
+    console.log(this.factory);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { PostModule } from './post/post.module';
-import { CodeModule } from './code/code.module';
-import { OpenaiModule } from './openai/openai.module';
-import { MessagesModule } from './messages/messages.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from './user/user.module';
+import {PostModule} from './post/post.module';
+import {CodeModule} from './code/code.module';
+import {OpenaiModule} from './openai/openai.module';
+import {MessagesModule} from './messages/messages.module';
+import {TestService} from "./Test.service";
 
 @Module({
   imports: [UserModule, PostModule, CodeModule, OpenaiModule, MessagesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    TestService,
+    {
+      provide: "Test",
+      useValue: ["a", "b", "c"]
+    },
+    {
+      provide: "Factory",
+      inject: [TestService],
+      useFactory(testService: TestService) {
+        console.log(testService.getHello());
+        return 123
+      }
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule {
+}
